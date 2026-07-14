dab6345eec71f6a2d61a9b805ef8eb818baaaae9
Nerdeen <52358769+nerdeen@users.noreply.github.com>
2026-07-14 14:43:40 +0300

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int [] result=new int[nums.length];
+        int evenIndex=0;
+        int oddIndex=1;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]>0){
+                result[evenIndex]=nums[i];
+                evenIndex+=2;
+            }
+            else{
+                result[oddIndex]=nums[i];
+                oddIndex+=2;
+            }
+        }
+        return result;
+    }
+}
